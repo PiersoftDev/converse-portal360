49f0728879cd9ab0a3f78222be516f2473fca75e
Mahendra <mahendra.cp1988@gmail.com>
2023-02-11 19:22:18 +0530

    Adding Additional Fields in Contact Information

--- src/components/vendor/company-contact-info.tsx
+++ src/components/vendor/company-contact-info.tsx
@@ -16,9 +16,14 @@ export const CompanyContactInfo = () => {
         display="grid"
         justifyContent="center"
       >
-        <TextField id="outlined-basic" label="Company Address" variant="outlined" required sx={{ mt: 3, width: "75ch" }} />
         <TextField id="outlined-basic" label="Company Email" variant="outlined" required sx={{ mt: 3, width: "75ch" }} InputProps={{ endAdornment: <Button variant="contained">Verify</Button> }} />
         <TextField id="outlined-basic" label="Company Mobile No" variant="outlined" required sx={{ mt: 3, width: "75ch" }} InputProps={{ endAdornment: <Button variant="contained">verify</Button> }} />
+        <TextField id="outlined-basic" label="company Address Lane 1" variant="outlined" required sx={{ mt: 3, width: "75ch" }} />
+        <TextField id="outlined-basic" label="company Address Lane 2" variant="outlined" required sx={{ mt: 3, width: "75ch" }} />
+        <TextField id="outlined-basic" label="city" variant="outlined" required sx={{ mt: 3, width: "40ch" }} />
+        <TextField id="outlined-basic" label="state" variant="outlined" required sx={{ mt: 3, width: "40ch" }} />
+        <TextField id="outlined-basic" label="postal Code" variant="outlined" required sx={{ mt: 3, width: "30ch" }} />
+        <TextField id="outlined-basic" label="country" variant="outlined" required sx={{ mt: 3, width: "30ch" }} />
       </Box>
     </React.Fragment>
   );
